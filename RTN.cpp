3780a49a07de8b9fd0f52974d2b253f7f0254c7f
OMAR <oahmed@etsy.com>
2014-10-12 01:14:04 -0400

    rtn insertcall :}

--- RTN.cpp
+++ RTN.cpp
@@ -2,8 +2,88 @@
 #include "pin.H"
 #include "RTN.h"
 
+void RTN_IPOINT_AFTER(char* name, PyObject* callback, long int return_value) {
+    PyObject* arguments = PyTuple_New(2);
+    PyTuple_SetItem(arguments, 0, PyString_FromString(name));
+    PyTuple_SetItem(arguments, 1, PyInt_FromLong(return_value));
+
+    PyObject_CallObject(callback, arguments);
+}
+
+void RTN_IPOINT_BEFORE(char* name, int num_args, PyObject* callback, long int arg0, long int arg1, long int arg2, long int arg3, long int arg4, long int arg5, long int arg6, long int arg7, long int arg8, long int arg9, long int arg10) {
+    PyObject* arguments = PyTuple_New(num_args+1);
+    PyTuple_SetItem(arguments, 0, PyString_FromString(name));
+    long int args[11];
+    args[0] = arg0;
+    args[1] = arg1;
+    args[2] = arg2;
+    args[3] = arg3;
+    args[4] = arg4;
+    args[5] = arg5;
+    args[6] = arg6;
+    args[7] = arg7;
+    args[8] = arg8;
+    args[9] = arg9;
+    args[10] = arg10;
+
+    for (int i=0; i < num_args; i++) {
+        PyTuple_SetItem(arguments, i+1, PyInt_FromLong(args[i]));
+    }
+
+    PyObject_CallObject(callback, arguments);
+}
+
 PyObject* Python_RTN_InsertCall(PyObject* self, PyObject* args) {
-    return Py_BuildValue("O", Py_False);
+    PyObject* num_args;
+    PyObject* function_name;
+    PyObject* rtn;
+    PyObject* callable;
+    PyObject* ipoint;
+
+    PyArg_ParseTuple(args, "L|O|L|L|O", &ipoint, &function_name, &rtn, &num_args, &callable);
+    if (!PyCallable_Check(callable) || (long int) num_args > 10 || (long int) num_args < 0) {
+        return Py_BuildValue("O", Py_False);
+    }
+
+    RTN rtn_object = *(RTN*) rtn;
+    if ((long int) ipoint == IPOINT_AFTER) {
+        RTN_InsertCall(rtn_object, IPOINT_AFTER, (AFUNPTR) RTN_IPOINT_AFTER,
+            IARG_ADDRINT, PyString_AsString(function_name),
+            IARG_PTR, callable,
+            IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+    } else if ((long int) ipoint == IPOINT_BEFORE) {
+        RTN_InsertCall(rtn_object, IPOINT_BEFORE, (AFUNPTR) RTN_IPOINT_BEFORE,
+            IARG_ADDRINT, PyString_AsString(function_name),
+            IARG_ADDRINT, num_args,
+            IARG_PTR, callable,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 1,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 2,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 3,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 4,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 5,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 6,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 7,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 8,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 9,
+            IARG_FUNCARG_ENTRYPOINT_VALUE, 10,
+            IARG_END);
+    } else {
+        return Py_BuildValue("O", Py_False);
+    }
+
+    return Py_BuildValue("O", Py_True);
+/*
+    for (int i=0; i < PyTuple_Size(args); i++) {
+        cur = PyTuple_GetItem(args, i);
+        if (PyString_Check(cur)) {
+            printf("[str] %s\n", PyString_AsString(cur));
+        } else if (PyInt_Check(cur)) {
+            printf("[int] %ld\n", PyInt_AsLong(cur));
+        }
+    }
+
+    return Py_BuildValue("O", Py_False);*/
 }
 
 PyObject* Python_RTN_Next(PyObject* self, PyObject* args) {
@@ -25,7 +105,6 @@ PyObject* Python_RTN_Prev(PyObject* self, PyObject* args) {
 }
 
 PyObject* Python_RTN_Invalid(PyObject* self, PyObject* args) {
-
     RTN* rtn_return = (RTN*) malloc(sizeof(RTN));
     *rtn_return = RTN_Invalid();
     return Py_BuildValue("L", rtn_return);
